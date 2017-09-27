4d0faab809c8e563ffbcbe581d406a7d9e4c4636
TrueCR <tylerg12@learn.cssd.ab.ca>
2017-09-27 13:12:06 -0600

    Fun Stuff

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function Music() {
-
-    alert("It works"); // Remove this line if you got a popup when you clicked the button. This just verifys that your js is linked.
-    
     
     //Break up text into individual sentences in the array "result"
    
