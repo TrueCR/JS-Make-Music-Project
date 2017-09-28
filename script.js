58b140181ff750583720f9851d0dc22d9bf734fb
TrueCR <tylerg12@learn.cssd.ab.ca>
2017-09-28 14:01:43 -0600

    gfd

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-// function music() {
-    //var textAreaInput=document.getElementById("TextBox").value;
-    //var output=document.querySelector("p");
-   // var result=document.getElementById("TextBox").value.match(/[^.!?]+[.!?]+/g);
-    //for (i = 0; i < result.length; i++ ) {
-        //if (i <= 2) 
-    //}
-//} 
-
 function highlight() {
-  var txt = document.querySelector('textarea').val();
+  var text = document.querySelector('textarea').val();
 
-  var sentences = txt.split('\.?!');
+  var sentences = text.split('\.?!');
   var html = '';
   sentences.forEach(function(sentence) {
     sentence = sentence.trim();
@@ -28,7 +19,7 @@ function highlight() {
     } else if (count < 20) {
       className = 'long';
     }
-    innerHTML += '<span class="'+className+'">'+sentence+'. </span>';
+    html += '<span class="'+className+'">'+sentence+'. </span>';
   });
 
   docment.getElementById('#output').innerHTML(html);
