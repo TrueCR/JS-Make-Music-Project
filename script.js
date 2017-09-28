b00009f760f81d0cd59ea57c19eb4634c51231b1
TrueCR <tylerg12@learn.cssd.ab.ca>
2017-09-28 13:23:38 -0600

    fedfds

---
+++ script.js
@@ -0,0 +1,8 @@
+// function music() {
+    //var textAreaInput=document.getElementById("TextBox").value;
+    //var output=document.querySelector("p");
+   // var result=document.getElementById("TextBox").value.match(/[^.!?]+[.!?]+/g);
+    //for (i = 0; i < result.length; i++ ) {
+        //if (i <= 2) 
+    //}
+//} 
