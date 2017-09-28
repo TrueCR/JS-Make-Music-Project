c577dec938815e36ac6746c1c5a2f1661458eb57
TrueCR <tylerg12@learn.cssd.ab.ca>
2017-09-28 13:50:52 -0600

    yrgfg

--- script.js
+++ script.js
@@ -6,3 +6,34 @@
         //if (i <= 2) 
     //}
 //} 
+
+function highlight() {
+  var txt = document.querySelector('textarea').val();
+
+  var sentences = txt.split('\.?!');
+  var html = '';
+  sentences.forEach(function(sentence) {
+    sentence = sentence.trim();
+    if (!sentence) {
+      return;
+    }
+    var className = 'longest';
+    var count = sentence.split(" ").length;
+    if (count < 3) {
+      className = 'shortest';
+    } else if (count < 5) {
+      className = 'short';
+    } else if (count == 5) {
+      className = 'medium';           
+    } else if (count < 20) {
+      className = 'long';
+    }
+    innerHTML += '<span class="'+className+'">'+sentence+'. </span>';
+  });
+
+  docment.getElementById('#output').innerHTML(html);
+    
+}
+
+highlight();
+document.querySelector('textarea').on('keyup', highlight);
