588de07cd504a449d95a17b1d647606c37145908
TrueCR <tylerg12@learn.cssd.ab.ca>
2017-10-03 13:55:40 -0600

    I dont even know anymore

--- script.js
+++
@@ -1,9 +0,0 @@
-function music() {
-var textAreaInput = document.getElementById("text").value;
-    var output = document.getElementById("testingp");
-    var result = document.getElementById('text').value.match( /[^.!?]+[.!?]+/g );
-    var words = textAreaInput.split(" ");    
-
-    alert(result[2].split(" ").length);  
-    
-}
